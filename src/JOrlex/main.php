e04a9bf540b9cfdd983664e1bd2a53d55c689600
JOrlex <41841553+JOrlex@users.noreply.github.com>
2018-08-05 19:54:31 -0700

    Update main.php

--- src/JOrlex/main.php
+++ src/JOrlex/main.php
@@ -6,7 +6,7 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 
-class Main extends PluginBase implements Listner {
+class Main extends PluginBase {
 
     public function onEnable(){
             $this->getLogger()->info("onEnable() has been called");
